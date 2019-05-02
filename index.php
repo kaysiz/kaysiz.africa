bbf99cf1f3dec4886cb38a447473c68e036b9d74
kaysiz <ksiziva@gmail.com>
2019-05-02 18:32:40 -0400

    u

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-hi
+<?php
+
+function solution($M, $A) {
+    $N = sizeof($A);
+    $count = array_pad(array(), $M + 1, 0);
+    $maxOccurence = 1;
+    $index = -1;
+    for ($i = 0; $i < $N; $i++) {
+        if ($count[$A[$i]] > 0) {
+            $tmp = $count[$A[$i]];
+            if ($tmp > $maxOccurence) {
+                $maxOccurence = $tmp;
+                $index = $i;
+            }
+            $count[$A[$i]] = $tmp + 1;
+        } else {
+            $count[$A[$i]] = 1;
+        }
+    }
+    return $A[$index];
+}
+solution(3, [1, 2, 3, 3, 1, 3, 1]);
+?>
